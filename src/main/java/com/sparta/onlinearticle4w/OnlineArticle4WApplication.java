c5f2a3b6a1fd9399687975af38d6b2376e391bd6
sgm26 <73274616+sgm26@users.noreply.github.com>
2022-02-01 09:37:09 +0900

    Initial commit

---
+++ src/main/java/com/sparta/onlinearticle4w/OnlineArticle4WApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.onlinearticle4w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineArticle4WApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineArticle4WApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/onlinearticle4w/OnlineArticle4WApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.onlinearticle4w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineArticle4WApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
